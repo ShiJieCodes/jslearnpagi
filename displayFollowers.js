1836fa6d5c82e775edc09dba3cb271cf1585108c
Lim Shi Jie <shijielim@gmail.com>
2023-08-20 19:29:42 +0800

    good learning sess

--- displayFollowers.js
+++ displayFollowers.js
@@ -1,3 +1,19 @@
-const display = () => {}
+import get from "./getElement.js";
+const container = get(".container");
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      // avartaur;/html url/login
+      const { avatar_url: avatar, html_url: link, login: name } = person;
+      return `<article class='card'>
+    <img src="${avatar}" alt="${name}" />
+    <h4>${name}</h4>
+    <a href="${link}}" class='btn'>view profile</a>
+    </article>`;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
+};
+
+export default display;
--- fetchFollowers.js
+++ fetchFollowers.js
@@ -1,5 +1,12 @@
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
 
-const fetchFollowers = async () => {}
+const fetchFollowers = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+  // data.map((obj) => {
+  //   const {} = obj
+  // })
+};
 
-export default fetchFollowers
+export default fetchFollowers;
